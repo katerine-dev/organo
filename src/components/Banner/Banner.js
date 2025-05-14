e8bebab92ba385e8250d3d2603b1cddf63ada523
katerine-dev <ka.lwitkoski@gmail.com>
2025-05-14 17:01:15 -0300

    Adiciona Banner

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,14 @@
+import './Banner.css'
+
+function Banner(){
+    return (
+        <header className='banner'>
+            <img src="/banner.png" alt="O banner principal da página do Organo" />
+        </header>
+    )
+}
+
+document.createElement('img')
+
+
+export default Banner
